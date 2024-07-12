41133b91d0852ed5105038ff12cf560f9cd2a4e1
hyunjae <dlguswo0504@naver.com>
2024-07-12 22:25:39 +0900

    hyunjae -- 도커파일 이미지 생성, 도커 컨테이너 up

---
+++ src/deploy.sh
@@ -0,0 +1,8 @@
+# 기동중인 lunch 도커 중단 및 삭제
+sudo docker ps -a -q --filter "name=salesforce-middleware" | grep -q . && docker stop salesforce-middleware && docker rm salesforce-middleware | true
+
+sudo docker rmi rheonik/salesforce-middleware:1.0
+
+sudo docker pull rheonik/salesforce-middleware:1.0
+
+docker run -d -p 8889:8889 --name lunch rheonik/salesforce-middleware:1.0
