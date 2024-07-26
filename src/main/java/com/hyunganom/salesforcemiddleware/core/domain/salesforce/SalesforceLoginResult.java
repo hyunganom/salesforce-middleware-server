931f114641ff06b7ce57133d0277c2827d44b6b0
hyunjae <dlguswo0504@naver.com>
2024-07-26 16:01:41 +0900

    hyunjae -- fix: code description SalesforceLoginResult 설명 주석 추가

--- src/main/java/com/hyunganom/salesforcemiddleware/core/domain/salesforce/SalesforceLoginResult.java
+++ src/main/java/com/hyunganom/salesforcemiddleware/core/domain/salesforce/SalesforceLoginResult.java
@@ -1,4 +1,3 @@
-// core/domain/SalesforceLoginResult.java
 package com.hyunganom.salesforcemiddleware.core.domain.salesforce;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -7,11 +6,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Salesforce 로그인 결과를 나타내는 클래스.
+ * 액세스 토큰과 인스턴스 URL을 포함.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
 public class SalesforceLoginResult {
+
     @JsonProperty(value = "access_token")
     private String accessToken;
 
