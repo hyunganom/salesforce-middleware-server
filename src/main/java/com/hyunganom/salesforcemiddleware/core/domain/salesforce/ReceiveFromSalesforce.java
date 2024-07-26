0eaad52e1a779224bb72b791d90268cc1cf1dcab
hyunjae <dlguswo0504@naver.com>
2024-07-26 14:34:06 +0900

    hyunjae -- fix: code description ReceiveFromSalesforce 설명 주석 추가

--- src/main/java/com/hyunganom/salesforcemiddleware/core/domain/salesforce/ReceiveFromSalesforce.java
+++ src/main/java/com/hyunganom/salesforcemiddleware/core/domain/salesforce/ReceiveFromSalesforce.java
@@ -3,15 +3,14 @@ package com.hyunganom.salesforcemiddleware.core.domain.salesforce;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
+/**
+ * Salesforce로부터 수신된 데이터를 나타내는 클래스.
+ * send__c 필드를 포함.
+ */
 @Data
-@Setter
-@Getter
 public class ReceiveFromSalesforce {
 
     @JsonProperty("send__c")
-    String send__c;
-
+    private String send__c;
 }
