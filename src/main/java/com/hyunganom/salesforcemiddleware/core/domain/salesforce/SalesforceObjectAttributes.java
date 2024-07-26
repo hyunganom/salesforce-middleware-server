96e612d2fbdf1661efd1cf87349ae2aeb2cb46b2
hyunjae <dlguswo0504@naver.com>
2024-07-26 15:55:39 +0900

    hyunjae -- fix: code description SalesforceObjectAttributes 설명 주석 추가

--- src/main/java/com/hyunganom/salesforcemiddleware/core/domain/salesforce/SalesforceObjectAttributes.java
+++ src/main/java/com/hyunganom/salesforcemiddleware/core/domain/salesforce/SalesforceObjectAttributes.java
@@ -1,4 +1,3 @@
-// core/domain/SalesforceAttributes.java
 package com.hyunganom.salesforcemiddleware.core.domain.salesforce;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -6,11 +5,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Salesforce 객체의 속성을 나타내는 클래스.
+ * 타입과 URL을 포함.
+ */
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-public class SalesforceAttributes {
+public class SalesforceObjectAttributes {
     private String type;
     private String url;
 }
